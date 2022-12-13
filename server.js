f617db6df911cd364f052e55f21a7c0925dcc2a7
Ngo Minh Phuong <minhphuong49th@gmail.com>
2022-12-13 17:00:14 +0700

    update 26

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS chuyên nghiệp 1 ${os.hostname()}</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS chuyên nghiệp 2 ${os.hostname()}</h1></body>`
   );
 });
 
